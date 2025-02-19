7f73e08374ff33b039d59d7f7bdd3673c61a801a
Samuel <samuel.aluizio@hotmail.com>
2025-02-19 20:53:44 -0300

    feat: add Spring Boot ✨

---
+++ src/main/java/com/dev/oralsin/doctorticket/DoctorTicketApplication.java
@@ -0,0 +1,16 @@
+package com.dev.oralsin.doctorticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author samueelg
+ */
+
+@SpringBootApplication
+public class DoctorTicketApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DoctorTicketApplication.class, args);
+    }
+}
--- src/main/java/com/dev/oralsin/doctorticket/controllers/BaseSeleniumController.java
+++ src/main/java/com/dev/oralsin/doctorticket/controllers/BaseSeleniumController.java
@@ -1,27 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.dev.oralsin.doctorticket.controllers;
 
+import java.time.Duration;
 import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author samueelg
  */
+
+@Component
 public abstract class BaseSeleniumController {
     
-    public void initGoogle(WebDriver driver){
+    private final WebDriver driver;  
+    private final WebDriverWait wait;
+    
+        @Autowired
+        public BaseSeleniumController(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+        
+         protected WebElement waitForElementToBeClickable(By by) {
+        return wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+         
+            protected void clickElement(By by) {
+        WebElement element = waitForElementToBeClickable(by);
+        if (element != null) {
+            element.click();
+        } else {
+            throw new RuntimeException("Elemento não encontrado: " + by);
+        }
+    }
+    
+    public void initGoogle(){
         Set<String> guiasAbertas = driver.getWindowHandles();
         String movideskAba = null;
         
@@ -39,7 +61,7 @@ public abstract class BaseSeleniumController {
             driver.switchTo().window(movideskAba);
         } else {
             // Se não encontrou, abre uma nova aba e acessa o site
-            driver.switchTo().newWindow(org.openqa.selenium.WindowType.TAB);
+            driver.switchTo().newWindow(WindowType.TAB);
             driver.get("https://oralsinf.movidesk.com/Ticket/Edit/125339");
             }
         }catch(Exception e){
@@ -47,37 +69,28 @@ public abstract class BaseSeleniumController {
             System.out.println("Não foi possível abrir o navegador, tente novamente.");
         }
     }
-    public void createTicket(WebDriver driver){
-        Actions actions = new Actions(driver);
-        actions.keyDown(Keys.ALT).sendKeys("t").keyUp(Keys.ALT).perform();
+    public void createTicket(){
+        new Actions(driver).keyDown(Keys.ALT).sendKeys("t").keyUp(Keys.ALT).perform();
     }
     
-    public void selectService(WebDriverWait wait){
-        WebElement seletorServico = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"ticket-sidebar\"]/div[2]/div[7]/div/a")));
-        seletorServico.click();
+    public void selectService(){
+        clickElement(By.xpath("//*[@id=\"ticket-sidebar\"]/div[2]/div[7]/div/a"));
     }
     
-    public void selectCategory(WebDriverWait wait){
-        WebElement seletorCategoria = wait.until(ExpectedConditions.elementToBeClickable(By.id("select2-chosen-17")));
-        seletorCategoria.click();
+    public void selectCategory(){
+        clickElement(By.xpath("select2-chosen-17"));
+        clickElement(By.xpath("//li[contains(@class, 'select2-result') and .//div[contains(normalize-space(), 'Solicitação de serviço')]]"));
+    }
+    
+    public void selectUrgency(){
+        clickElement(By.xpath("select2-chosen-21"));
             
-        WebElement opcao = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@class, 'select2-result') and .//div[contains(normalize-space(), 'Solicitação de serviço')]]")));
-        opcao.click();    
-    }
-    
-    public void selectUrgency(WebDriverWait wait){
-        WebElement seletorUrgencia = wait.until(ExpectedConditions.elementToBeClickable(By.id("select2-chosen-21")));
-        seletorUrgencia.click();
-            
-        WebElement opcService = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@class, 'select2-result')]//div[contains(text(), 'Baixa')]")));
-        opcService.click();
+        clickElement(By.xpath("//li[contains(@class, 'select2-result')]//div[contains(text(), 'Baixa')]"));
     }  
     
-    public void selectSector(WebDriverWait wait){     
-        WebElement seletorSetor = wait.until(ExpectedConditions.elementToBeClickable(By.id("select2-chosen-31")));
-        seletorSetor.click();
+    public void selectSector(){     
+        clickElement(By.xpath("select2-chosen-31"));
             
-        WebElement opcService = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@class, 'select2-result')]//div[contains(normalize-space(), 'Recepção')]")));
-        opcService.click();
+        clickElement(By.xpath("//li[contains(@class, 'select2-result')]//div[contains(normalize-space(), 'Recepção')]"));
     } 
 }
